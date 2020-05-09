20021bfdc8c91f2d9d5114cb6857da883045882c
KevinTram <thekevintram@gmail.com>
2020-05-09 03:47:16 -0400

    formatting

--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,15 @@
 fun main() {
     var episode = 1
+    
     print("Enter First Episode ID: ")
     var episodeID = readLine()!!.toInt()
+
     print("Enter Show ID: ")
     val showID = readLine()!!.toInt()
+
     print("Enter Number of Episodes: ")
     val episodeCount = readLine()!!.toInt()
+
     repeat(episodeCount) {
         println("https://storage.googleapis.com/auengine.appspot.com/$showID/sub/${episode++}_${episodeID++}.mp4")
     }
