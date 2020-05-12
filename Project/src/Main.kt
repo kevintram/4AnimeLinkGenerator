750a6ae54e7d39ae60cdfd0f0b4b901fbf5c0834
KevinTram <thekevintram@gmail.com>
2020-05-12 00:53:03 -0400

    User now only needs to paste the link of the first episode they want to watch, program now checks if the initial link and the generated links are valid

--- Project/src/Main.kt
+++ Project/src/Main.kt
@@ -1,31 +1,61 @@
-import java.lang.IllegalArgumentException
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+
 
 fun main(args: Array<String>) {
 
-    println()
-    var episode = 1
+    print("Enter Link of First Episode To Download: ")
+    var firstLink = readLine()!!
 
-    print("Enter First Episode ID: ")
-    var episodeID = readLine()!!.toInt()
+    var isValidUrl = false
+    while (!isValidUrl) {
+        if (firstLink.contains("https://storage.googleapis.com/auengine.appspot.com") || !urlIsMp4(firstLink)) {
+            isValidUrl = true
+        } else {
+            print("Please enter a valid url from the auengine data base: ")
+            firstLink = readLine()!!
+        }
+    }
 
-    print("Enter Show ID: ")
-    val showID = readLine()!!.toInt()
+    if (firstLink.contains('?')) {//if link contains google access id
+        firstLink = firstLink.substringBefore('?')
+    }
+    //split link
+    val linkSplit = firstLink.split('/')
+    //get juicy info
+    val showID = linkSplit[4].toInt()
+    val vocalTrack = linkSplit[5]
+    var episode = linkSplit[6][0].toInt() - 48
+    var episodeID = linkSplit[6].substringAfter('_').substringBefore('.').toInt()
 
-    print("Enter Number of Episodes: ")
+    print("Enter Number of Episodes To Download: ")
     val episodeCount = readLine()!!.toInt()
-
-    print("Sub or Dub?: ")
-    val vocalTrack: String = readLine()!!.trim().toLowerCase()
-    if (vocalTrack != "sub" && vocalTrack != "dub")
-        throw IllegalArgumentException("Please enter sub or dub")
 
     println()
 
     println("----- LINKS -----")
 
-    repeat(episodeCount) {
-        println("https://storage.googleapis.com/auengine.appspot.com/$showID/$vocalTrack/${episode++}_${episodeID++}.mp4")
+    var link: String
+    for (i in 1..episodeCount) {
+        link = "https://storage.googleapis.com/auengine.appspot.com/$showID/$vocalTrack/${episode++}_${episodeID++}.mp4"
+        if (urlIsMp4(link)) {
+            println(link)
+        } else {
+            println("Link #$i is not valid. Putting a stop to printing links.")
+            break
+        }
+
     }
 
     println()
+}
+
+fun urlIsMp4(url: String): Boolean {
+    return try {
+        BufferedReader(InputStreamReader(URL(url).openConnection().getInputStream()))
+        true
+    }  catch (e: Exception) {
+        false
+    }
 }
