ee3af5bedb62299dd9bb3ab0784562b0ff739038
KevinTram <thekevintram@gmail.com>
2020-05-12 22:58:40 -0400

    user can download the files

--- Project/src/Main.kt
+++ Project/src/Main.kt
@@ -1,11 +1,10 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.*
 import java.net.URL
 
 
 fun main(args: Array<String>) {
 
-    print("Enter Link of First Episode To Download: ")
+    print("Enter Link of First Episode: ")
     var firstLink = readLine()!!
 
     var isValidUrl = false
@@ -17,10 +16,6 @@ fun main(args: Array<String>) {
             firstLink = readLine()!!
         }
     }
-
-    if (firstLink.contains('?')) {//if link contains google access id
-        firstLink = firstLink.substringBefore('?')
-    }
     //split link
     val linkSplit = firstLink.split('/')
     //get juicy info
@@ -29,31 +24,61 @@ fun main(args: Array<String>) {
     var episode = linkSplit[6][0].toInt() - 48
     var episodeID = linkSplit[6].substringAfter('_').substringBefore('.').toInt()
 
-    print("Enter Number of Episodes To Download: ")
+    print("Enter Number of Episodes You Want To Retrieve: ")
     val episodeCount = readLine()!!.toInt()
 
     println()
 
     println("----- LINKS -----")
 
-    var link: String
+    val links = ArrayList<String>()
     for (i in 1..episodeCount) {
-        link = "https://storage.googleapis.com/auengine.appspot.com/$showID/$vocalTrack/${episode++}_${episodeID++}.mp4"
+        val link = "https://storage.googleapis.com/auengine.appspot.com/$showID/$vocalTrack/${episode++}_${episodeID++}.mp4"
         if (urlIsMp4(link)) {
-            println(link)
+            links.add(link)
+            println(links.last())
         } else {
             println("Link #$i is not valid. Putting a stop to printing links.")
             break
         }
+    }
 
+    val toDownload = askYesOrNo(question = "Do you want to download these episodes?")
+
+    if (toDownload) {
+        print("Enter the path of the folder you want to download to: ")
+        val path = readLine()!!
+        for (i in 0 until links.size) {
+            println("\nDownloading Video #${i+1}...")
+            downloadVideo(links[i],path)
+            println("Finished Downloading Video #${i+1}!")
+            println()
+        }
     }
 
     println()
 }
 
 /**
+ * continually prompts for a yes or no answer until the user types in "yes" or "no" (case doesn't matter)
+ * returns false if the answer is no
+ * returns true if the answer is yes
+ */
+fun askYesOrNo(question: String): Boolean {
+    print("\n$question Enter yes or no: ")
+    var answer = readLine()!!.toLowerCase().trim()
+    while(true) {
+        if (answer == "yes" || answer == "no" ) {
+            return answer == "yes"
+        } else {
+            print("\nPlease answer yes or no: ")
+            answer = readLine()!!.toLowerCase().trim()
+        }
+    }
+}
+/**
  * checks if a url links to a valid .mp4 file
- * inputStream will throw a file io exception if url does not link to a valid .mp4
+ * val inputStream will throw a file io exception if url does not link to a valid .mp4
  */
 fun urlIsMp4(url: String): Boolean {
     return try {
@@ -61,5 +86,33 @@ fun urlIsMp4(url: String): Boolean {
         true
     }  catch (e: Exception) {
         false
+    }
+}
+
+/**
+ * downloads the mp4 file to the given path
+ */
+fun downloadVideo(url: String, path: String) {
+    try {
+        //get input stream from url
+        val bufferedInputStream = BufferedInputStream(URL(url).openConnection().getInputStream())
+        //create file
+        val name = url.substringAfterLast('/').substringBefore('.')
+        val file = File("$path/$name.mp4")
+        //write to file
+        val fileOutputStream = FileOutputStream(file.path)
+        var count: Int
+        val buffer = ByteArray(4 * 1024)
+        count = bufferedInputStream.read(buffer)
+
+        while(count != -1) {
+            fileOutputStream.write(buffer,0,count)
+            count = bufferedInputStream.read(buffer)
+        }
+        //save file
+        file.createNewFile()
+    }  catch (e: Exception) {
+        println("An error occurred")
+        e.printStackTrace()
     }
 }
